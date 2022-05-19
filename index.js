95b52da226311e7e095f64d8fdd252867991e0e2
Viktor Edman <viktor@edman.se>
2022-05-19 15:42:45 +0200

    did some stuf

--- index.js
+++ index.js
@@ -15,18 +15,41 @@ app.get('/chat', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+function listConnections() {
+  io.fetchSockets()
+  .then(sockets => console.log("Currently connected sockets", 
+    sockets.map(socket => {
+      return {
+                id: socket.id,
+                name: socket?.name,
+                ip: socket.IP,
+                connected: socket.connected}
+              })
+    ))
+}
+
 const chatLog = []
 
 io.on('connection', (socket) => {
-    io.fetchSockets().then(sockets => console.log("Currently connected sockets", sockets.map(socket => socket.id)))
+    socket.IP = socket.handshake.headers["x-real-ip"];
+
     console.log('a user connected '+socket.id);
-    console.log(chatLog)
+    listConnections();
     io.to(socket.id).emit('chat history', chatLog)
-    console.log(socket.handshake.headers["x-real-ip"]);
+
+    
+    
     socket.on('chat message', (msg) => {
+      if (msg.includes('/clear') && msg.substr(0, 5).toLowerCase() === "admin") {
+          chatLog.length=0
+          socket.emit("chat history", chatLog)
+          return
+        }
+      socket.name = msg.split(':')[0]
       console.log('message: ' + msg);
       chatLog.push(msg)
       io.emit('chat message', msg);
+      listConnections();
     });
   });
 
